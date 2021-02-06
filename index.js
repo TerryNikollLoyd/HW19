fc4d2eb1c488044296b729018273832971d64754
TerryNikollLoyd <63319070+TerryNikollLoyd@users.noreply.github.com>
2021-02-06 18:47:28 -0500

    js
    js

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-console.log("Your index.js file is loaded correctly!"); 
+console.log("click"); 
 
+;
+//  $("p").click();
 
-// $("p").click();
+ $("button").click(function(){
+      console.log("click");
+      $(this).css ("color","white"); 
+   });
 
-// $("p").click(function(){
-//     // action goes here!!
-//   });
+   $("body").click(function(){
+    $("button").css("color","white");
+ });
 
 //   $(document).ready(function(){
 //     $("p").click(function(){
 //       $(this).hide();
-//     });
+//     })
 //   });
 
   $(document).ready(function () {
     $('body.hidden').fadeIn(1000).removeClass('hidden');
 });
 
+{
+  color:red
+}
